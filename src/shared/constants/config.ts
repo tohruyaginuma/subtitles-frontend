3bc15dad215760d87e05a1f3ccad772fbd25138b
Tohru Yaginuma <toru45s@gmail.com>
2025-08-12 08:06:24 -0700

    update debug log

--- src/shared/constants/config.ts
+++ src/shared/constants/config.ts
@@ -1,8 +1,13 @@
 import packageJson from "../../../package.json";
 
 export const VERSION = packageJson.version;
-export const APP_ROOT = process.env.APP_ROOT || "http://localhost:3000";
 export const API_ROOT = process.env.API_ROOT || "http://127.0.0.1:8000";
 export const API_ROOT_V1 = process.env.API_ROOT_V1 || `${API_ROOT}/v1`;
 export const WS_ROOT = process.env.WS_ROOT || "ws://localhost:8000";
 export const IS_DEV = process.env.NODE_ENV === "development";
+
+console.log("API_ROOT", API_ROOT);
+console.log("API_ROOT_V1", API_ROOT_V1);
+console.log("WS_ROOT", WS_ROOT);
+console.log("IS_DEV", IS_DEV);
+console.log("VERSION", VERSION);
