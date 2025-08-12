f2106a88570260c02e9450a382e04efc00ed3575
Tohru Yaginuma <toru45s@gmail.com>
2025-08-11 19:59:43 -0700

    update next setting

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   compress: false,
+  async headers() {
+    return [
+      { source: "/api/:path*", headers: [{ key: "Alt-Svc", value: "clear" }] }, // H3広告を無効化（Cloudflareが上書きする場合も）
+    ];
+  },
 };
 
 export default nextConfig;
